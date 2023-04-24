9ccc1714d6805294b8a7f64a6d7d52bfcb6739bc
DESKTOP-VPFP9CK\yousa <karipu-@hotmail.co.jp>
2023-04-24 23:10:02 +0900

    password ハッシュ化

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -93,7 +93,7 @@ class UsersController extends Controller
         ->update(
                 ['username' =>$up_username 
                 ,'mail' =>$up_mail 
-                ,'password' =>$up_password 
+                ,'password' => bcrypt($up_password) 
                 ,'bio' =>$up_bio 
                 ,'images' =>$fileName]
         );
@@ -114,7 +114,7 @@ class UsersController extends Controller
                  ->update(
                         ['username' =>$up_username 
                         ,'mail' =>$up_mail 
-                        ,'password' =>$up_password 
+                        ,'password' =>bcrypt($up_password) 
                         ,'bio' =>$up_bio 
                         ]
                         );
